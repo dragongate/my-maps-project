e87fb5d3bfdbb1d3bceb52e011c409a3bd62814d
Chatmongkone Porkrue <chatmongkone@mycostech.com>
2017-02-07 17:23:18 +0700

    Add service function and use async/await

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { AgmCoreModule } from 'angular2-google-maps/core';
 import { MapsService } from './maps.service';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/toPromise';
+
 interface marker {
   lat: number;
   lng: number;
@@ -24,21 +27,25 @@ export class AppComponent {
   address: string;
   addresses: string[] = [
     "香港新界	西貢	曉嵐閣	3座5樓C室",
-    " International Commerce Centre	1 Austin Road West	Kowloon, Hong Kong",
-    "	Rw. 005, Kelurahan Krukut,	Kecamatan Taman Sari	Kotamadya Jakarta Barat, Indonesia",
+    "International Commerce Centre	1 Austin Road West	Kowloon, Hong Kong",
+    "Rt.12	Rw.5, Kelurahan Krukut,	Kecamatan Taman Sari	Kotamadya Jakarta Barat, Indonesia",
     "Changqing Garden ,	Dongxihu District, Wuhan Hubei Province PRC",
-    "Dubai	Dubai Internet City 12 #02-211	P O Box 502430, Dubai	United Arab Emirates",
-    "No. 68 Xue Fu South Road+Changqing Garden Street+Dongxihu District Wuhan Hubei Province"
+    "Dubai	Dubai Internet City 12 #02-211	P O Box 502430, Dubai	United Arab Emirates"
   ];
+
   zoom: number = 8;
 
 
   optional_address: string;
-  location: string;
+  locality: string;
   street_address: string;
   route: string;
   country: string;
   post_box: string;
+  postal_code: string;
+  street_number: string;
+  floor: string;
+  room: string;
 
   detail: string;
 
@@ -48,14 +55,14 @@ export class AppComponent {
     this.search();
   }
   search() {
- 
+
     this.addresses.forEach(data => {
       this.mapsService.getmapsCoordinate(data).subscribe(
         x => {
           console.log("VALUE RECEIVED: ", x);
           console.log(JSON.stringify(x));
           let data = x;
-          if(data.status == "OK"){
+          if (data.status == "OK") {
             console.log(data.results[0].geometry.location.lat);
             this.markers.push({
               lat: data.results[0].geometry.location.lat,
@@ -67,7 +74,7 @@ export class AppComponent {
             this.detail = data.results[0].formatted_address;
             console.log(this.markers);
           }
-          
+
 
         }
       );
@@ -76,44 +83,57 @@ export class AppComponent {
 
   }
 
-    searchMore() {
- 
-
-      this.mapsService.getmapsCoordinate(this.address).subscribe(
-        x => {
-          console.log("VALUE RECEIVED: ", x);
-          console.log(JSON.stringify(x));
-          let data = x;
-          if(data.status != "OK"){
-            console.error("Search not found");
-          }
-          else{
-            console.log(data.results[0].geometry.location.lat);
-            
-            this.markers.push({
-              lat: data.results[0].geometry.location.lat,
-              lng: data.results[0].geometry.location.lng,
-              detail: data.results[0].formatted_address
-            });
-            this.lat = data.results[0].geometry.location.lat;
-            this.lng = data.results[0].geometry.location.lng;
-            this.detail = data.results[0].formatted_address;
-            console.log("this.marker : " + this.markers);
-            this.addresses.push(this.detail);
-          }
-          
-          
+  searchMore() {
 
 
-        },
-        error => {
+    this.mapsService.getmapsCoordinate(this.address).subscribe(
+      x => {
+        console.log("VALUE RECEIVED: ", x);
+        console.log(JSON.stringify(x));
+        let data = x;
+        if (data.status != "OK") {
           console.error("Search not found");
         }
-      );
+        else {
+          console.log(data.results[0].geometry.location.lat);
 
+          this.markers.push({
+            lat: data.results[0].geometry.location.lat,
+            lng: data.results[0].geometry.location.lng,
+            detail: data.results[0].formatted_address
+          });
+          this.lat = data.results[0].geometry.location.lat;
+          this.lng = data.results[0].geometry.location.lng;
+          this.detail = data.results[0].formatted_address;
+          console.log("this.marker : " + this.markers);
+          this.addresses.push(this.detail);
+        }
 
+      },
+      error => {
+        console.error("Search not found");
+      }
+    );
 
   }
+
+  search_loop() {
+    let status = "OK!";
+    // while (status != "OK") {
+    for (let i = 0; i < 4; i++) {
+      let result = this.mapsService.getmapsCoordinate2(this.address);
+      //this.mapsService.getmapsCoordinate2(this.address);
+      console.log(JSON.stringify(result));
+      console.log(result);
+      result.then(data => {
+        console.log(data.status);
+      });
+      // result._zone_symbol_value.status
+      if (status == "OK!")
+        break;
+    }
+  }
+
   search_optional() {
     if (this.address) {
       this.optional_address = this.address;
@@ -128,15 +148,47 @@ export class AppComponent {
     if (this.post_box) {
       this.optional_address = this.optional_address + "|" + "post_box:" + this.post_box;
     }
+    if (this.street_address) {
+      this.optional_address = this.optional_address + "|" + "street_address:" + this.street_address;
+    }
+    if (this.locality) {
+      this.optional_address = this.optional_address + "|" + "locality:" + this.locality;
+    }
+    if (this.postal_code) {
+      this.optional_address = this.optional_address + "|" + "postal_code:" + this.postal_code;
+    }
+    if (this.street_number) {
+      this.optional_address = this.optional_address + "|" + "post_box:" + this.street_number;
+    }
+    if (this.floor) {
+      this.optional_address = this.optional_address + "|" + "floor:" + this.floor;
+    }
+    if (this.room) {
+      this.optional_address = this.optional_address + "|" + "room:" + this.room;
+    }
+
     this.mapsService.getmapsCoordinate(this.optional_address).subscribe(
       x => {
         console.log("VALUE RECEIVED: ", x);
         console.log(JSON.stringify(x));
         let data = x;
-        console.log(data.results[0].geometry.location.lat);
-        this.lat = data.results[0].geometry.location.lat;
-        this.lng = data.results[0].geometry.location.lng;
-        this.detail = data.results[0].formatted_address;
+        if (data.status != "OK") {
+          console.error("Search not found");
+        }
+        else {
+          console.log(data.results[0].geometry.location.lat);
+
+          this.markers.push({
+            lat: data.results[0].geometry.location.lat,
+            lng: data.results[0].geometry.location.lng,
+            detail: data.results[0].formatted_address
+          });
+          this.lat = data.results[0].geometry.location.lat;
+          this.lng = data.results[0].geometry.location.lng;
+          this.detail = data.results[0].formatted_address;
+          console.log("this.marker : " + this.markers);
+          this.addresses.push(this.detail);
+        }
       }
     );
 
